2c82f0f2cb27e527adc9f5682d10a403a305e50c
Yush Kapoor <YushDotKapoor@gmail.com>
2024-12-10 18:18:43 -0500

    Created 3 test cases for historical Tests for the correct number of entries. Tests the first and last 5 entries of the historical data to make sure dataset is sorted in the correct order. I chose not to cross check the entire dataset to prevent bloating. We can reasonable assume based on the first and last five entries that method works as expected

--- tests/test_cnn.py
+++ tests/test_cnn.py
@@ -37,6 +37,34 @@ class FetchFearGreedIndexTest(absltest.TestCase):
         )
         self.assertEqual(got, want)
 
+    def test_historical(self):
+        with open(FAKE_JSON_RESPONSE_GOLDEN_FILE, "rt") as fd:
+            # Lambda is evaluated lazily, force JSON load while file is open.
+            content = json.load(fd)
+            fake_fetcher = lambda: content
+        got = cnn.historical(fetcher=fake_fetcher)
+
+        self.assertEqual(len(got), 269)
+        
+        first_five = [
+            cnn.FearGreedIndex(51.03333333333334, "neutral", datetime.datetime.fromtimestamp(1619395200.0)),
+            cnn.FearGreedIndex(52.79999999999999, "neutral", datetime.datetime.fromtimestamp(1619481600.0)),
+            cnn.FearGreedIndex(54.133333333333326, "neutral", datetime.datetime.fromtimestamp(1619568000.0)),
+            cnn.FearGreedIndex(58.93333333333334, "greed", datetime.datetime.fromtimestamp(1619654400.0)),
+            cnn.FearGreedIndex(49.26666666666667, "neutral", datetime.datetime.fromtimestamp(1619740800.0))
+        ]
+        
+        self.assertEqual(got[:5], first_five)
+
+        last_five = [
+            cnn.FearGreedIndex(41.1246, "fear", datetime.datetime.fromtimestamp(1650585600.0)),
+            cnn.FearGreedIndex(40.1512, "fear", datetime.datetime.fromtimestamp(1650672000.0)),
+            cnn.FearGreedIndex(40.1512, "fear", datetime.datetime.fromtimestamp(1650758400.0)),
+            cnn.FearGreedIndex(30.8254, "fear", datetime.datetime.fromtimestamp(1650844800.0)),
+            cnn.FearGreedIndex(30.8254, "fear", datetime.datetime.fromtimestamp(1650903669.254))
+        ]
+
+        self.assertEqual(got[-5:], last_five)
 
 if __name__ == "__main__":
     absltest.main()
